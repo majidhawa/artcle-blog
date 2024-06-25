d7f7f07e9b83d647b48c3b2e2ab7f24802a45162
majidhawa <majidhawaah@gmail.com>
2024-06-25 16:14:03 +0300

    first commit

---
+++ app/src/main/java/com/akirachix/artcileblog/Article.kt
@@ -0,0 +1,11 @@
+package com.akirachix.artcileblog
+
+data class Article(
+    val title: String,
+    val author: String,
+    val authorProfilePicture: String,
+    val publishingDate: String,
+    val articlePreview: String,
+    val articlePhoto: String,
+    val link: String
+)
